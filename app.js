acd967408bcb53fa78c157ea718c9f6dc38e9a5a
Rumeel Hussain <rumeelhussain123@gmail.com>
2019-12-14 18:35:06 +0400

    Saving data through Mongoose

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const errorController = require('./controllers/error');
 //const mongoConnect = require('./util/database').mongoConnect;
-const User = require('./models/user');
+//const User = require('./models/user');
 
 const app = express();
 
@@ -17,14 +17,14 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-  User.findById('5df3e12a7c58630c44cd28ed')
-    .then(user => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch(err => console.log(err));
-});
+// app.use((req, res, next) => {
+//   User.findById('5df3e12a7c58630c44cd28ed')
+//     .then(user => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch(err => console.log(err));
+// });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -32,7 +32,7 @@ app.use(shopRoutes);
 app.use(errorController.get404);
 
 mongoose
-  .connect('mongodb+srv://rumeel:rumi123@cluster0-itlhv.azure.mongodb.net/test?retryWrites=true&w=majority'
+  .connect('mongodb+srv://rumeel:rumi123@cluster0-itlhv.azure.mongodb.net/shop?retryWrites=true&w=majority'
   )
   .then(result => {
     app.listen(3000);
